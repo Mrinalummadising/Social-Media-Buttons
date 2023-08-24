f564a50ef3a62b9659dcde620ec2dbb44e1b0d1b
Mrinalummadising <mrinalummadising.com>
2023-08-24 11:48:36 +0000

    Social Media buttons

---
+++ index.js
@@ -0,0 +1,20 @@
+const Button = props => {
+  const {className, buttonText} = props
+  
+  return <button className={`button ${className}`}>{buttonText}</button>
+}
+
+const element = (
+  <div className="social-buttons-container">
+    <div className="content-container">
+      <h1 className="heading">Social Buttons</h1>
+      <div className="buttons-container">
+        <Button buttonText="Like" className="like-button" />
+        <Button buttonText="Comment" className="comment-button" />
+        <Button buttonText="Share" className="share-button" />
+      </div>
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
